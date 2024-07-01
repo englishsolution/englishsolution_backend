06cfa9cfe28e4bf491aec6cfa2eb6b793123ff19
ohseongbin <dannybin814@naver.com>
2024-07-02 02:33:20 +0900

    api 연결 및 .env 설정, api 시험용으로 임의로 url 변경함

--- djangoProject/app_video/urls.py
+++ djangoProject/app_video/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('chatbot', views.query_view_chatbot)
+    # path('chatbot', views.query_view_chatbot)
+    path('chatbot',views.chatbot)
 ]
--- djangoProject/app_video/views.py
+++ djangoProject/app_video/views.py
@@ -2,17 +2,19 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Test
 from dotenv import load_dotenv
+from openai import OpenAI
 import os
-import openai
 
-load_dotenv(verbose=True) #env 파일에서 api_key를 가져옴
+load_dotenv(verbose=True) #env 파일에서 api_key를 가져옴 # 배포시 verbose 지우기
 
-openai.api_key = os.getenv("OPENAI_API_KEY") #openai api key 연결
+API_KEY=os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=API_KEY)
 
 def chatbot(request):
-    print(request) # to check
-    query = openai.ChatCompletion.create(
-        models="gpt-3.5-turbo",
+    request = "give me 5 words randomly"
+    query = client.chat.completions.create(
+        model="gpt-3.5-turbo",
         messages=[
             {"role": "user", "content": request}
         ],
@@ -20,16 +22,16 @@ def chatbot(request):
         stop=None, # 대화의 종류를 명시, None -> 종료 조건 x
         temperature=0.5, #0~2사이의 값, 높을 수록 더 랜덤한 response 발생, 낮을 수록 보다 정확한 정보 제공
     )
-    response = query.choices[0].messages["content"]
+    response = query.choices[0].message.content
     print(response)
-    return response
+    
 
-def query_view_chatbot(request):
-    if request.method == "POST":
-        prompt = str(request.POST["prompt"])
-        response = chatbot(prompt)
-        return JsonResponse({'response':response})
-    return render(request, "app_video/chatbot.html")
+# def query_view_chatbot(request):
+#     if request.method == "POST":
+#         prompt = str(request.GET["prompt"])
+#         response = chatbot(prompt)
+#         return JsonResponse({'response':response})
+#     return render(request, "app_video/chatbot.html")
 
 
 # Create your views here.
