50034ea94dee48d317de84bff8dfceba57a1d339
ohseongbin <dannybin814@naver.com>
2024-07-03 03:00:14 +0900

    chatbot 대화의 연속성 구현 ( 기존의 것은 질문하고 다음 질문을 하면 그 이전 질문을 반영하지 않았음) chatbot 메서드는 request를 받아서 response를 return하게 추후 수정하기

--- djangoProject/app_video/views.py
+++ djangoProject/app_video/views.py
@@ -1,30 +1,42 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from .models import Test
 from dotenv import load_dotenv
 from openai import OpenAI
 import os
 
-load_dotenv(verbose=True) #env 파일에서 api_key를 가져옴 # 배포시 verbose 지우기
-
-API_KEY=os.getenv("OPENAI_API_KEY")
-
+load_dotenv(verbose=True) #env 파일에서 api_key를 가져옴 ######## 배포시 verbose 지우기
+API_KEY = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=API_KEY)
 
-def chatbot(request):
-    request = "give me 5 words randomly"
-    query = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": request}
-        ],
-        max_tokens=1024, # chat 에서 생성해낼 수 있는 최대 tokens 수
-        stop=None, # 대화의 종류를 명시, None -> 종료 조건 x
-        temperature=0.5, #0~2사이의 값, 높을 수록 더 랜덤한 response 발생, 낮을 수록 보다 정확한 정보 제공
-    )
-    response = query.choices[0].message.content
-    print(response)
-    
+#request 를 받아 response를 응답하는 chatbot
+def chatbot(request, message_history=[]):
+    for i in range(2):
+        request= input("prompt 입력: ")
+
+        message_history.append(  # to consecutive message
+            {
+                "role": "user",  # add user's question to messege_history
+                "content": request,
+            })
+
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages= message_history,
+            max_tokens=1024, # chat 에서 생성해낼 수 있는 최대 tokens 수
+            stop=None, # 대화의 종류를 명시, None -> 종료 조건 x
+            temperature=0.5, #0~2사이의 값, 높을 수록 더 랜덤한 response 발생, 낮을 수록 보다 정확한 정보 제공
+        )
+
+        message_history.append(
+            {
+                "role":"assistant", "content": response.choices[0].message.content,
+            }
+        )
+        print(message_history[-1])
+       # return message_history[-1]
+    return HttpResponse(message_history)
+
 
 # def query_view_chatbot(request):
 #     if request.method == "POST":
