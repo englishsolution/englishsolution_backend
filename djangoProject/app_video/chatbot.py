db8502c0d98ecf7d5e88fadebf89f814dd18fe31
ohseongbin <dannybin814@naver.com>
2024-07-05 14:06:38 +0900

    통신 임시 실행을 위한 설정

--- djangoProject/app_video/chatbot.py
+++ djangoProject/app_video/chatbot.py
@@ -8,44 +8,46 @@ load_dotenv(verbose=True) #env 파일에서 api_key를 가져옴 ######## 배포
 API_KEY = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=API_KEY)
 
+message_history= []
+
 #chatbot을 열면 실행되는 메소드
-def chatbot(request, message_history=[]):
-    while(True):
-        request = input("prompt 입력: ") # 임시 prompt
+def chatbot(request, message_history):
 
-        if request == "exit":
-            break
+    if request == "exit":
+        return
 
-        message_history.append(  # to consecutive message
-            {
-                "role": "user",  # add user's question to messege_history
-                "content": request,
-            })
+    message_history.append(  # to consecutive message
+        {
+            "role": "user",  # add user's question to messege_history
+            "content": request,
+        })
 
-        response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages= message_history,
-            max_tokens=1024, # chat 에서 생성해낼 수 있는 최대 tokens 수
-            stop=None, # 대화의 종류를 명시, None -> 종료 조건 x
-            temperature=0.5, #0~2사이의 값, 높을 수록 더 랜덤한 response 발생, 낮을 수록 보다 정확한 정보 제공
-        )
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=message_history,
+        max_tokens=1024,  # chat 에서 생성해낼 수 있는 최대 tokens 수
+        stop=None,  # 대화의 종류를 명시, None -> 종료 조건 x
+        temperature=0.5,  # 0~2사이의 값, 높을 수록 더 랜덤한 response 발생, 낮을 수록 보다 정확한 정보 제공
+    )
 
-        message_history.append(
-            {
-                "role": "assistant", "content": response.choices[0].message.content,
-            }
-        )
-        print(message_history[-1]["content"])
-       # return message_history[-1]["content"]
-    return HttpResponse(message_history[-1]["content"])
+    message_history.append(
+        {
+            "role": "assistant", "content": response.choices[0].message.content,
+        }
+    )
+    print("in chatbot method:", message_history[-1]["content"])
 
 
-# def query_view_chatbot(request):
-#     if request.method == "POST":
-#         prompt = str(request.GET["prompt"])
-#         response = chatbot(prompt)
-#         return JsonResponse({'response':response})
-#     return render(request, "app_video/chatbot.html")
+    return message_history[-1]["content"]
+
+
+def request_to_chatbot(request):
+    if request.method == "POST":
+        prompt = str(request.POST["prompt"])
+        response = chatbot(prompt, message_history)
+        print("in requesttochatbot:", response)
+        return JsonResponse({'response': response})
+
 
 
 
--- djangoProject/app_video/urls.py
+++ djangoProject/app_video/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from . import chatbot,views
 
 urlpatterns = [
-    # path('chatbot', views.query_view_chatbot)
-    path('chatbot', chatbot.chatbot),
+    path('chatbot', chatbot.request_to_chatbot),
     path('', views.test)
 ]
