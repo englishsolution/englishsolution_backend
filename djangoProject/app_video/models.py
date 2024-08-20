13d7dd38dedaacf94d24be64f82bc84c10980aeb
AzureAD\nayeaun <gad122838@gmail.com>
2024-08-20 16:32:58 +0900

    video_identify 칼럼 추가. 중복 칼럼 이름 장고에서 바뀌었음

--- djangoProject/app_video/models.py
+++ djangoProject/app_video/models.py
@@ -1,10 +1,3 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 from django.conf import settings
 
@@ -154,7 +147,7 @@ class Video(models.Model):
         on_delete=models.CASCADE
     )
     script = models.TextField(default='default_script')
-
+    video_identitfy = models.CharField(unique=True, max_length=45)
     class Meta:
         db_table = 'video'
 
@@ -189,6 +182,8 @@ class SentenceQuiz(models.Model):
 
     class Meta:
         db_table = 'sentence_quiz'
+
+
 
 class Word(models.Model):
     word_id = models.AutoField(primary_key=True)
