02f933a9753c5ed74c784735c05b8d8c8f5cfbdf
AzureAD\nayeaun <gad122838@gmail.com>
2024-08-20 16:34:36 +0900

    구조 조정

--- djangoProject/app_video/urls.py
+++ djangoProject/app_video/urls.py
@@ -10,7 +10,9 @@ router = DefaultRouter()
 router.register(r'sentences', SentenceViewSet)
 router.register(r'words',WordViewSet)
 
-
+from django.contrib import admin
+from django.urls import path
+from app_video import view, speaking, scripts, quiz, tests, user
 
 urlpatterns = [
     path('testserver',view.testserver),
@@ -25,8 +27,7 @@ urlpatterns = [
     path('processing_speaking/', speaking.processing_speaking, name='processing_speaking'),
     path('all_sentence_quiz/', quiz.all_sentence_quiz, name='all_sentence_quiz'),
     path('all_word_quiz/', quiz.all_word_quiz, name='all_word_quiz'),
-    path('replay_word_quiz/', quiz.replay_word_quiz, name='replay_word_quiz'),
-    path('replay_sentence_quiz/', quiz.replay_sentence_quiz, name='replay_sentence_quiz'),
+    path('replay_quiz/', quiz.replay_quiz, name='replay_quiz'),
     path('request_to_chatbot/', tests.request_to_chatbot, name='request_to_chatbot'),
     path('signup/', user.signup, name='signup'),
     path('activate/<uidb64>/<token>/', user.activate, name='activate'),
@@ -34,6 +35,7 @@ urlpatterns = [
     path('logout/', user.logout, name='logout'),
     path('login/', user.login, name='login'),
     path('delete/', user.delete, name='delete'),
+
+    path('quiz_index/', quiz.quiz_index, name='quiz_index'),
+
 ]
-
-
